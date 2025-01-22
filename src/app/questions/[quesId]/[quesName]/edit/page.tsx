9180e51878f30a9172a9da7468a5a41cea1a72f8
Ritesh kumar sah <riteshkumar555sah@gmail.com>
2025-01-22 13:22:40 +0530

    bug fixing

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,17 +1,17 @@
-import { db, questionCollection } from '@/models/name';
-import { databases } from '@/models/server/config';
 import React from 'react';
 import EditQues from './EditQues';
+import { db, questionCollection } from '@/models/name';
+import { databases } from '@/models/server/config';
 
 interface PageProps {
-  params: {
+  params: Promise<{
     quesId: string;
     quesName: string;
-  };
+  }>;
 }
 
 const Page: React.FC<PageProps> = async ({ params }: PageProps) => {
-  const { quesId } = params;
+  const { quesId } = await params;
   const question = await databases.getDocument(db, questionCollection, quesId);
 
   return <EditQues question={question} />;
