5ea33a93ac0348a2c6979bbc19cad0aa68762315
Ritesh kumar sah <riteshkumar555sah@gmail.com>
2025-05-13 01:06:24 +0530

    image rendering bugs again

--- src/app/questions/Search.tsx
+++ src/app/questions/Search.tsx
@@ -30,9 +30,7 @@ const Search = () => {
 
     try {
       // Construct new URL with updated search param
-      const newSearchParams = new URLSearchParams(
-        searchParams?.toString() || ''
-      );
+      const newSearchParams = new URLSearchParams(searchParams?.toString());
       newSearchParams.set('search', search);
 
       // Push updated search query to the router
@@ -47,7 +45,7 @@ const Search = () => {
 
   const clearSearch = () => {
     setSearch('');
-    const newSearchParams = new URLSearchParams(searchParams?.toString() || '');
+    const newSearchParams = new URLSearchParams(searchParams?.toString());
     newSearchParams.delete('search');
     router.push(`${pathname}?${newSearchParams.toString()}`);
   };
