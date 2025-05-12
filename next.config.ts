95f7bd4eb608bf7c2f68f12d08426d9bfaa8375c
Ritesh kumar sah <riteshkumar555sah@gmail.com>
2025-05-13 01:11:47 +0530

    image rendering bugs again

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // Disable ESLint during builds
-  },
   images: {
     remotePatterns: [
       {
